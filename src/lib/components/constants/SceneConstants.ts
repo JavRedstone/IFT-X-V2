32d86bc9de88e09d23cee985406a93ec3122956c
JavRedstone <javier.huang168@gmail.com>
2024-05-07 22:31:08 -0400

    update less lag

--- src/lib/components/constants/SceneConstants.ts
+++ src/lib/components/constants/SceneConstants.ts
@@ -1,5 +1,5 @@
 export class SceneConstants {
-    public static readonly DELTA: number = 0.01;
+    public static readonly DELTA: number = 1/60;
     public static readonly SPEEDUP_MIN: number = 0;
     public static readonly SPEEDUP_MAX: number = 100;
     public static readonly SPEEDUP_STEP: number = 1;
